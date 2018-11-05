51c2a6110754c1fce3bbc7738a6b14c137f6186f
Gary Marigliano <gmarigliano93@gmail.com>
2018-11-05 13:54:10 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-cmake "-DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" -DCMAKE_BUILD_TYPE=Debug .. 
+mkdir -p build
+cd build
+cmake  -DCMAKE_BUILD_TYPE=Debug .. 
 # cmake "-DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" ..
 cmake --build .
